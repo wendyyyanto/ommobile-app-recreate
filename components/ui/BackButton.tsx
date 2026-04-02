68bfee28b1c9f7f7b6f2d2075da782f19122ac58
wendyyyanto <wendyyyanto@gmail.com>
2026-04-02 13:05:29 +0700

    back button default onPress

--- components/ui/BackButton.tsx
+++ components/ui/BackButton.tsx
@@ -1,9 +1,10 @@
 import { Image } from "expo-image";
+import { router } from "expo-router";
 import { Pressable } from "react-native";
 
-const BackButton = ({ onPress }: { onPress: () => void }) => {
+const BackButton = ({ onPress }: { onPress?: () => void }) => {
 	return (
-		<Pressable onPress={onPress}>
+		<Pressable onPress={onPress || (() => router.back())}>
 			<Image
 				source={require("@/assets/icons/arrow_back.svg")}
 				style={{ width: 32, height: 32 }}
