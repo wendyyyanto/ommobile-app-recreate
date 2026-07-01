8dfc2bd7dbf3a75efc655581f9f50c94e4b09bab
wendyyyanto <wendyyyanto@gmail.com>
2026-07-01 22:39:49 +0700

    fix: main screen white background

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,4 +1,4 @@
-import { router, Tabs, usePathname } from "expo-router";
+import { router, Tabs, useSegments } from "expo-router";
 
 import colors from "@/constants/colors";
 import fonts from "@/constants/fonts";
@@ -12,9 +12,8 @@ const tabBarIconSize = { width: 18, height: 18, marginBottom: 6 };
 
 export default function TabsLayout() {
 	const insets = useSafeAreaInsets();
-	const pathname = usePathname();
-	const tabBarRootRoutes = ["/", "/teachings", "/notifications"];
-	const shouldHideTabBar = !tabBarRootRoutes.includes(pathname);
+	const segments = useSegments();
+	const shouldHideTabBar = segments.length > 2;
 	const baseTabBarStyle = {
 		backgroundColor: "black",
 		paddingBottom: 12 + insets.bottom,
@@ -33,147 +32,147 @@ export default function TabsLayout() {
 
 	return (
 		<Tabs
-					screenOptions={{
-						headerTransparent: true,
-						headerShadowVisible: false,
-						headerTitleStyle: {
-							color: "white"
-						},
-						headerTintColor: "white",
-						sceneStyle: {
-							backgroundColor: "black"
-						},
-						tabBarStyle: shouldHideTabBar
-							? [{ ...baseTabBarStyle }, { display: "none" }]
-							: baseTabBarStyle,
-						tabBarLabelStyle: {
-							...fonts.caption1White
-						}
-					}}
-				>
-					<Tabs.Screen
-						name="index"
-						options={{
-							title: "Home",
-							tabBarLabel: ({ focused }) => (
-								<Text
-									numberOfLines={2}
-									adjustsFontSizeToFit
-									minimumFontScale={0.8}
-									style={[
-										fonts.caption1White,
-										{
-											color: focused
-												? colors.lightBlue
-												: colors.white
-										}
-									]}
-									className="text-center"
-								>
-									Home
-								</Text>
-							),
-							tabBarIcon: ({ focused }) =>
-								focused ? (
-									<Image
-										source={require("@/assets/icons/home_active.svg")}
-										style={tabBarIconSize}
-									/>
-								) : (
-									<Image
-										source={require("@/assets/icons/home_inactive.svg")}
-										style={tabBarIconSize}
-									/>
-								),
-							headerShown: false
-						}}
-					/>
-					<Tabs.Screen
-						name="teachings"
-						listeners={{
-							tabPress: (e) => {
-								e.preventDefault();
-								router.replace("/teachings");
-							}
-						}}
-						options={{
-							title: "Teachings",
-							tabBarLabel: ({ focused }) => (
-								<Text
-									numberOfLines={2}
-									adjustsFontSizeToFit
-									minimumFontScale={0.8}
-									style={[
-										fonts.caption1White,
-										{
-											color: focused
-												? colors.lightBlue
-												: colors.white
-										}
-									]}
-									className="text-center"
-								>
-									Teachings
-								</Text>
-							),
-							tabBarIcon: ({ focused }) =>
-								focused ? (
-									<Image
-										source={require("@/assets/icons/teaching_active.svg")}
-										style={tabBarIconSize}
-									/>
-								) : (
-									<Image
-										source={require("@/assets/icons/teaching_inactive.svg")}
-										style={tabBarIconSize}
-									/>
-								),
-							headerShown: false
-						}}
-					/>
-					<Tabs.Screen
-						name="notifications"
-						listeners={{
-							tabPress: (e) => {
-								e.preventDefault();
-								router.replace("/notifications");
-							}
-						}}
-						options={{
-							title: "Notifications",
-							tabBarLabel: ({ focused }) => (
-								<Text
-									numberOfLines={2}
-									adjustsFontSizeToFit
-									minimumFontScale={0.8}
-									style={[
-										fonts.caption1White,
-										{
-											color: focused
-												? colors.lightBlue
-												: colors.white
-										}
-									]}
-									className="text-center"
-								>
-									Notifications
-								</Text>
-							),
-							tabBarIcon: ({ focused }) =>
-								focused ? (
-									<Image
-										source={require("@/assets/icons/notif_active.svg")}
-										style={tabBarIconSize}
-									/>
-								) : (
-									<Image
-										source={require("@/assets/icons/notif_inactive.svg")}
-										style={tabBarIconSize}
-									/>
-								),
-							headerShown: false
-						}}
-					/>
-			</Tabs>
+			screenOptions={{
+				headerTransparent: true,
+				headerShadowVisible: false,
+				headerTitleStyle: {
+					color: "white"
+				},
+				headerTintColor: "white",
+				sceneStyle: {
+					backgroundColor: "black"
+				},
+				tabBarStyle: shouldHideTabBar
+					? { display: "none" }
+					: baseTabBarStyle,
+				tabBarLabelStyle: {
+					...fonts.caption1White
+				}
+			}}
+		>
+			<Tabs.Screen
+				name="index"
+				options={{
+					title: "Home",
+					tabBarLabel: ({ focused }) => (
+						<Text
+							numberOfLines={2}
+							adjustsFontSizeToFit
+							minimumFontScale={0.8}
+							style={[
+								fonts.caption1White,
+								{
+									color: focused
+										? colors.lightBlue
+										: colors.white
+								}
+							]}
+							className="text-center"
+						>
+							Home
+						</Text>
+					),
+					tabBarIcon: ({ focused }) =>
+						focused ? (
+							<Image
+								source={require("@/assets/icons/home_active.svg")}
+								style={tabBarIconSize}
+							/>
+						) : (
+							<Image
+								source={require("@/assets/icons/home_inactive.svg")}
+								style={tabBarIconSize}
+							/>
+						),
+					headerShown: false
+				}}
+			/>
+			<Tabs.Screen
+				name="teachings"
+				listeners={{
+					tabPress: (e) => {
+						e.preventDefault();
+						router.replace("/teachings");
+					}
+				}}
+				options={{
+					title: "Teachings",
+					tabBarLabel: ({ focused }) => (
+						<Text
+							numberOfLines={2}
+							adjustsFontSizeToFit
+							minimumFontScale={0.8}
+							style={[
+								fonts.caption1White,
+								{
+									color: focused
+										? colors.lightBlue
+										: colors.white
+								}
+							]}
+							className="text-center"
+						>
+							Teachings
+						</Text>
+					),
+					tabBarIcon: ({ focused }) =>
+						focused ? (
+							<Image
+								source={require("@/assets/icons/teaching_active.svg")}
+								style={tabBarIconSize}
+							/>
+						) : (
+							<Image
+								source={require("@/assets/icons/teaching_inactive.svg")}
+								style={tabBarIconSize}
+							/>
+						),
+					headerShown: false
+				}}
+			/>
+			<Tabs.Screen
+				name="notifications"
+				listeners={{
+					tabPress: (e) => {
+						e.preventDefault();
+						router.replace("/notifications");
+					}
+				}}
+				options={{
+					title: "Notifications",
+					tabBarLabel: ({ focused }) => (
+						<Text
+							numberOfLines={2}
+							adjustsFontSizeToFit
+							minimumFontScale={0.8}
+							style={[
+								fonts.caption1White,
+								{
+									color: focused
+										? colors.lightBlue
+										: colors.white
+								}
+							]}
+							className="text-center"
+						>
+							Notifications
+						</Text>
+					),
+					tabBarIcon: ({ focused }) =>
+						focused ? (
+							<Image
+								source={require("@/assets/icons/notif_active.svg")}
+								style={tabBarIconSize}
+							/>
+						) : (
+							<Image
+								source={require("@/assets/icons/notif_inactive.svg")}
+								style={tabBarIconSize}
+							/>
+						),
+					headerShown: false
+				}}
+			/>
+		</Tabs>
 	);
 }
